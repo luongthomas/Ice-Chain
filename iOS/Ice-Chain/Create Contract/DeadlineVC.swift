20d645b338a517dea8e79966d860fc0c0a1b6afd
Thomas Luong <Luongt@me.com>
2018-09-09 20:43:20 -0700

    Add datepicker for deadline selection in contract

--- iOS/Ice-Chain/Create Contract/DeadlineVC.swift
+++ iOS/Ice-Chain/Create Contract/DeadlineVC.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2018 Thomas Luong. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class DeadlineVC: UIViewController {
+    
+    var contract = Contract()
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func continueButton(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MMM yyyy"
+        let selectedDate = dateFormatter.string(from: datePicker.date)
+        print(selectedDate)
+
+        // Set contract Date
+        contract.deadline = datePicker.date
+
+        // get parent view controller
+        let parentVC = self.parent as! CreateContractVC
+
+        // change page of PageViewController
+        let nextPage = [parentVC.orderedViewControllers[3]]
+        parentVC.setViewControllers(nextPage, direction: .forward, animated: true, completion: nil)
+    }
+    
+}
