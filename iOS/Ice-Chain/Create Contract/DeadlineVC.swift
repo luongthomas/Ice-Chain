a240be2166b810b4cd4223bc25c4ec31b438e4ec
Thomas Luong <Luongt@me.com>
2018-09-09 20:42:50 -0700

    Selecting predefined temp range options will now highlight the option and allow user to press continue button to move on.

---
+++ iOS/Ice-Chain/Create Contract/DeadlineVC.swift
@@ -0,0 +1,9 @@
+//
+//  DeadlineVC.swift
+//  Ice-Chain
+//
+//  Created by Thomas Luong on 9/9/18.
+//  Copyright © 2018 Thomas Luong. All rights reserved.
+//
+
+import Foundation
--- iOS/Ice-Chain/Create Contract/TemperatureRangeVC.swift
+++ iOS/Ice-Chain/Create Contract/TemperatureRangeVC.swift
@@ -11,15 +11,34 @@ import UIKit
 class TemperatureRangeVC: UIViewController, UITextFieldDelegate {
 
     var contract = Contract()
+    
+    // TODO: Add label to top of screen to show current selection
 
     @IBOutlet weak var minTempRangeTextField: TextField!
     @IBOutlet weak var maxTempRangeTextField: TextField!
+    @IBOutlet weak var prechosenOptions: UIStackView!
     
+    let niceBlue = UIColor(red: 66/255, green: 137/255, blue: 247/255, alpha: 1)
+    
+    func resetAllBtnColors() {
+        for case let button as UIButton in self.prechosenOptions.subviews {
+            button.backgroundColor = .white
+            button.setTitleColor(niceBlue, for: .normal)
+        }
+    }
     
     @IBAction func tempSelect(_ sender: Any) {
         guard let button = sender as? UIButton else {
             return
         }
+        
+        // Only do reset colors for the buttons that have preselected temp ranges
+        if (0...5).contains(button.tag) {
+            resetAllBtnColors()
+            button.backgroundColor = niceBlue
+            button.setTitleColor(.white, for: .normal)
+        }
+    
         
         switch button.tag {
         case 0:
@@ -103,7 +122,20 @@ class TemperatureRangeVC: UIViewController, UITextFieldDelegate {
         self.present(alert, animated: true, completion: nil)
     }
     
+    @IBAction func handleContinue(_ sender: Any) {
+        // get parent view controller
+        let parentVC = self.parent as! CreateContractVC
+        
+        // change page of PageViewController
+        let nextPage = [parentVC.orderedViewControllers[2]]
+        parentVC.setViewControllers(nextPage, direction: .forward, animated: true, completion: nil)
+    }
+    
     @IBAction func unwindToGlobal(segue: UIStoryboardSegue) {
         
     }
+    
+    
+    
+    
 }
