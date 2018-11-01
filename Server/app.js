c04fb8dc6f6d741265302f9978cf697402a91fed
Thomas Luong <Luongt@me.com>
2018-11-01 15:31:25 -0700

    Add one of each type of contract

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var qtum = require("qtumjs-wallet");
+var abi = require('ethereumjs-abi')
+
 
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/ContractDB';
@@ -50,7 +52,8 @@ app.get("/database-insert", (req, res, next) => {
 		var wrappedBuyerAddr =  "\"" + buyerAddr + "\""
 		var wrappedSellerAddr = "\"" + sellerAddr + "\""
 
-		var cursor = db.collection('Contracts').insertOne({
+		var cursor = db.collection('Contracts').insertMany([
+		{
 			"_id": 9,
 			"contractName": "Lotion Shipment",
 			"description": "Lotion",
@@ -65,8 +68,65 @@ app.get("/database-insert", (req, res, next) => {
 			"deadline": 1540773606,
 			"depositLimit": 1,
 			"status": 0,
-			"cargoValue": 10
-		})
+			"cargoValue": 10,
+			"contractAddress": "bb3f3c1ca886eb988c1d4dc979def855d63f77ee"
+		},
+		{
+			"_id": 10,
+			"contractName": "Intel CPU Shipment",
+			"description": "Intel CPUs",
+			"depositorName": "Seller",
+			"depositorEmail": "Seller@gmail.com",
+			"depositorAddress":  wrappedSellerAddr,
+			"otherPartyName": "Seller",
+			"otherPartyAddress":  wrappedBuyerAddr,
+			"otherPartyEmail": "Seller@gmail.com",
+			"minTemperature": 10,
+			"maxTemperature": 90,
+			"deadline": 1540774000,
+			"depositLimit": 2,
+			"status": 1,
+			"cargoValue": 20,
+			"contractAddress": "bb3f3c1ca886eb988c1d4dc979def855d63f77ee"
+		},
+		{
+			"_id": 11,
+			"contractName": "Ice Cream Shipment",
+			"description": "Ice Cream",
+			"depositorName": "Buyer",
+			"depositorEmail": "Buyer@gmail.com",
+			"depositorAddress":  wrappedBuyerAddr,
+			"otherPartyName": "Seller",
+			"otherPartyAddress":  wrappedSellerAddr,
+			"otherPartyEmail": "Seller@gmail.com",
+			"minTemperature": -10,
+			"maxTemperature": 10,
+			"deadline": 1540773808,
+			"depositLimit": 3,
+			"status": 2,
+			"cargoValue": 5,
+			"contractAddress": "bb3f3c1ca886eb988c1d4dc979def855d63f77ee"
+		},
+		{
+			"_id": 12,
+			"contractName": "Hot Dog Shipment",
+			"description": "Hot Dog",
+			"depositorName": "Seller",
+			"depositorEmail": "Seller@gmail.com",
+			"depositorAddress":  wrappedSellerAddr,
+			"otherPartyName": "Buyer",
+			"otherPartyAddress":  wrappedBuyerAddr,
+			"otherPartyEmail": "Buyer@gmail.com",
+			"minTemperature": 20,
+			"maxTemperature": 60,
+			"deadline": 1540774098,
+			"depositLimit": 5,
+			"status": 3,
+			"cargoValue": 1,
+			"contractAddress": "bb3f3c1ca886eb988c1d4dc979def855d63f77ee"
+		}
+
+		])
 
 		db.collection('Contracts').find({_id: 9}).toArray(function(err, items) {
 			res.send(JSON.stringify({items}))
@@ -76,6 +136,7 @@ app.get("/database-insert", (req, res, next) => {
 
 app.listen(5124, () => {
 	// main()
+	convertDataToAbi()
 	// walletInfo().catch((err) => console.log(err))
  	console.log("Server running on port 5124");
 });
@@ -90,6 +151,13 @@ async function main() {
 	console.log("mnemonic:", mnemonic)
 	console.log("public address:", wallet.address)
 	console.log("private key (WIF):", wallet.toWIF())
+}
+
+function convertDataToAbi() {
+	var parameterTypes = ["address", "uint256", "bool"];
+	var parameterValues = ["0x1234567812345678", "0x314159268", true];
+	var encoded = abi.rawEncode(parameterTypes, parameterValues);
+	console.log(encoded.toString('hex'));
 }
 
 async function walletInfo() {
